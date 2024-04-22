8186dcffdeee74756bbd5a46bd07185f09cada75
alejandro luna <alejandro9425@gmail.com>
2024-04-22 16:23:09 -0500

    autenticacion

--- src/Routers/AppRoutes.tsx
+++ src/Routers/AppRoutes.tsx
@@ -1,10 +1,37 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {onAuthStateChanged} from 'firebase/auth'
+import {useAppDispatch, useAppSelector} from 'Store/hooks';
+import {selectAuthInfo, setAuth} from 'Store/slices';
 import RoutersMain from './RoutersMain';
+import { auth } from 'Config';
+
 
 export const AppRoutes = () => {
+  const dispatch = useAppDispatch();
+  const {Auth: {isAuthenticated, isLoading}} = useAppSelector(selectAuthInfo);
+
+  useEffect(() => {
+    
+    onAuthStateChanged(auth, async user => {
+      dispatch(setAuth({isAuthenticated: false, isLoading: true}));
+      if(user) {
+        dispatch(setAuth({isAuthenticated: true, isLoading: false}));
+console.log(user)
+        return; 
+      }
+      dispatch(setAuth({isAuthenticated: false, isLoading: false}));
+    });
+  
+    return () => {}
+  }, [dispatch])
+  
+
+  if(isLoading) return <>Cargando.....</>
+
   return (
     <Router>
-      <RoutersMain isAthenticated={true} />
+      <RoutersMain isAthenticated={isAuthenticated} />
     </Router>
   )
 }
--- src/Submodules/auth/pages/login/login.page.tsx
+++ src/Submodules/auth/pages/login/login.page.tsx
@@ -1,8 +1,49 @@
-
-
+import { useState } from 'react';
+import {signInWithEmailAndPassword} from 'firebase/auth';
+import {auth} from 'Config';
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+const provider = new GoogleAuthProvider();
 export const LoginPage = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClick = () => {
+    signInWithEmailAndPassword(auth,email, password).then((response) => {
+      console.log(response)
+    })
+    .catch(e => console.log(e))
+  }
+
+  const onSignInGoogle = () => {
+    signInWithPopup(auth, provider)
+    .then((response) => {
+      console.log(response)
+    }).catch((e) => console.log(e))
+  }
+
   return (
-    <>LoginPage</>
+    <>
+      <input 
+        type="email" 
+        placeholder="email" 
+        name='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input 
+        type="password" 
+        placeholder="password" 
+        name='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button 
+        type="button"
+        onClick={() => handleClick()}
+      >Iniciar</button>
+      <label onClick={() => onSignInGoogle()} >Iniciar con google</label>
+    </>
   )
 }
 
