81958a57a6664b5d0e35a96e5c99ba9e1c5f4b33
Stefany Cardenas Martinez <stefany.cardenas@pragma.com.co>
2024-12-15 04:04:01 -0500

    refactor: change anchor by a Link on 404 page

--- src/Submodules/404/pages/404/404.page.tsx
+++ src/Submodules/404/pages/404/404.page.tsx
@@ -1,12 +1,13 @@
 import { Layout } from "shared/components/templates/layout/layout";
 import "./404.page.scss";
+import { Link } from "react-router-dom";
 
 export const PageNotFound = () => {
   return (
     <Layout className="page-404">
       <h1>404</h1>
       <p>Oops... Parece que la página que buscas no existe.</p>
-      <a href="/">Regresar al inicio</a>
+      <Link to="/">Regresar al inicio</Link>
     </Layout>
   );
 };
