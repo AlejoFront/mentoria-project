e538cf154e583330bf58bf766d5f34e0f1865cb1
Stefany Cardenas Martinez <stefany.cardenas@pragma.com.co>
2024-12-15 05:02:41 -0500

    feat: add new field text to googlebutton component

--- src/Shared/components/atoms/googleButton/googleButton.component.tsx
+++ src/Shared/components/atoms/googleButton/googleButton.component.tsx
@@ -4,13 +4,14 @@ import googleIcon from 'shared/assets/img/google-icon.svg';
 
 type GoogleButtonProps = {
   onClick: () => void;
+  text?: string;
 };
 
-export const GoogleButton: React.FC<GoogleButtonProps> = ({ onClick }) => {
+export const GoogleButton: React.FC<GoogleButtonProps> = ({ onClick, text }) => {
   return (
     <button className="google-button" onClick={onClick}>
       <img src={googleIcon} alt="Google icon" />
-      <span>Iniciar sesión con Google</span>
+      <span>{text ?? "Iniciar sesión con Google"}</span>
     </button>
   );
 };
