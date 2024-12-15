ec205cb3e0c8b4f03b66ae7fb515761451ba01b5
Stefany Cardenas Martinez <stefany.cardenas@pragma.com.co>
2024-12-15 05:04:25 -0500

    chore: remove unused methods of homeprivate page

--- src/Submodules/admin/pages/home/homePrivate.page.tsx
+++ src/Submodules/admin/pages/home/homePrivate.page.tsx
@@ -1,20 +1,9 @@
-import { auth } from "config";
-import { signOut } from "firebase/auth";
-import { Link } from "react-router-dom";
 import { Layout } from "shared/components/templates/layout/layout";
 
 export const HomePrivate = () => {
-  const onSignOut = async () => {
-    await signOut(auth);
-  };
   return (
     <Layout>
       <h1>Home Admin</h1>
-      <br />
-      <Link to={"/profile"}>Profile</Link>
-      <button type="button" onClick={() => onSignOut()}>
-        Salir
-      </button>
     </Layout>
   );
 };
