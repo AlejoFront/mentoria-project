2cd8d89991c8e2fe70cc8de71d336738017516b3
Stefany Cardenas Martinez <stefany.cardenas@pragma.com.co>
2024-12-15 05:54:26 -0500

    feat: persist langange change

--- src/Shared/context/language.context.tsx
+++ src/Shared/context/language.context.tsx
@@ -1,41 +1,57 @@
-import {createContext, useState, useContext, ReactNode, FC} from 'react';
+import {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  FC,
+  useEffect,
+} from "react";
 
-import en from 'shared/locale/en.json';
-import es from 'shared/locale/es.json';
+import en from "shared/locale/en.json";
+import es from "shared/locale/es.json";
 
-type Language = 'en' | 'es';
+type Language = "en" | "es";
 
 interface LanguageContextType {
-    language: Record<string, any>;
-    swtchLanguage: (lng: Language) => void;
+  language: Record<string, any>;
+  swtchLanguage: (lng: Language) => void;
 }
 
-const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
+const LanguageContext = createContext<LanguageContextType | undefined>(
+  undefined
+);
 
 const languages = {
-    en,
-    es
-}
+  en,
+  es,
+};
 
 interface LanguageProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export const LanguageProvider: FC<LanguageProviderProps> = ({children}) => {
-    const [language, setLanguage] = useState<Language>('es');
-    const swtchLanguage = (lng:Language) => setLanguage(lng);
+export const LanguageProvider: FC<LanguageProviderProps> = ({ children }) => {
+  const [language, setLanguage] = useState<Language>(() => {
+    return (localStorage.getItem("lng") as Language) || "es";
+  });
+  const swtchLanguage = (lng: Language) => setLanguage(lng);
 
-    return (
-        <LanguageContext.Provider value={{language: languages[language], swtchLanguage}} >
-            {children}
-        </LanguageContext.Provider>
-    );
-}
+  useEffect(() => {
+    localStorage.setItem("lng", language);
+  }, [language]);
+  return (
+    <LanguageContext.Provider
+      value={{ language: languages[language], swtchLanguage }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+};
 
 export const useLanguage = () => {
-    const context = useContext(LanguageContext);
-    if(!context){
-        throw new Error('useLanguage mus be used with in a LanguageProvider');
-    }
-    return context;
-}
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error("useLanguage mus be used with in a LanguageProvider");
+  }
+  return context;
+};
